37890655f6612c1cde8edadea6fc85eed79f3b28
gschweber12 <gs494@njit.edu>
2020-06-24 16:08:35 -0400

    Feature: Setup calculator with addition and subtraction csv files

--- src/CSVTests.py
+++ src/CSVTests.py
@@ -7,12 +7,12 @@ class MyTestCase(unittest.TestCase):
         self.csv_reader = CsvReader('employee_birthday.txt')
 
 
-def test_return_data_as_objects(self):
-    people = self.csv_reader.return_data_as_objects('person')
-    self.assertIsInstance(people, list)
-    test_class = ClassFactory('person', self.csv_reader.data[0])
-    for person in people:
-        self.assertEqual(person.__name__, test_class.__name__)
+    def test_return_data_as_objects(self):
+        people = self.csv_reader.return_data_as_objects('person')
+        self.assertIsInstance(people, list)
+        test_class = ClassFactory('person', self.csv_reader.data[0])
+        for person in people:
+            self.assertEqual(person.__name__, test_class.__name__)
 
 
 if __name__ == '__main__':
