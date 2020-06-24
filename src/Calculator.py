3f6895dd37ce0a172d35ed4b8c8a741100db8be6
gschweber12 <gs494@njit.edu>
2020-06-24 10:46:34 -0400

    Feature: Setup calculator with multiplication

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,7 +2,8 @@ def addition(a, b):
     return a + b
 def subtraction(a, b):
     return a - b
-
+def multiplication(a, b):
+    return a * b
 class Calculator:
     result = 0
 
@@ -15,4 +16,8 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
         return self.result
