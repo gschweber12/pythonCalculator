4f913910a75932bbc4051b96b4d9527262a90868
gschweber12 <gs494@njit.edu>
2020-06-24 10:23:16 -0400

    Feature: Setup calculator with add method

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,7 +1,13 @@
+def addition(a, b):
+    return a + b
+
+
 class Calculator:
     result = 0
 
     def _init_(self):
-        x = 2 + 2
-        self.result = x
+
         pass
+    def add(self, a, b):
+        self.result = a + b
+        return addition(a, b)
