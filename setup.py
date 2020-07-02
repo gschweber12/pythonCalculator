63313de15f235a8946d16deacb3537af638c9218
gschweber12 <gs494@njit.edu>
2020-07-02 15:02:02 -0400

    task: fixing readme

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version='0.01',
+      description='Its a calculator',
+      author= '',
+      author_email='',
+      url='',
+      packages=find_packages(),
+      )
