fb183ba5cc8b3cf72718c9a5ae24461b82f7c3ee
gschweber12 <50150260+gschweber12@users.noreply.github.com>
2020-07-08 13:53:03 -0400

    Update Statistics.py

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -2,6 +2,9 @@ from Calculator.Calculator import Calculator
 from Statistics.Mean import mean
 from Statistics.SampleMean import sample_mean
 from Statistics.Median import median
+from Statistics.StandardDeviation import standard_deviation
+from Statistics.Zscore import z_score
+from Statistics.Mode import mode
 
 class Statistics(Calculator):
 
@@ -16,3 +19,16 @@ class Statistics(Calculator):
     def median(self):
         self.result = median(self.data)
         return self.result
+    
+    def mode(self):
+        self.result = mode(self.data)
+        return self.result
+
+    def standard_deviation(self):
+        self.result = standard_deviation(self.data)
+        return self.result
+
+    def z_score(self, score):
+        self.result = z_score( score, self.data)
+        return self.result
+
